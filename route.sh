fc50da3ac8ff91a11aae319899f685787605aeb1
Xiaotao Liang <418xiaotao@163.com>
2015-06-05 23:19:21 +0800

    add sources.list & route rules

---
+++ route.sh
@@ -0,0 +1,6 @@
+route add -net 10.0.0.0 netmask 255.0.0.0 dev eth0
+route add -net 61.129.42.0 netmask 255.255.255.0 dev eth0
+route add -net 175.186.0.0 netmask 255.255.0.0 dev eth0
+route add -net 192.168.0.0 netmask 255.255.0.0 dev eth0
+route add -net 202.120.0.0 netmask 255.255.0.0 dev eth0
+
